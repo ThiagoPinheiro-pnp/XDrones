7cae2c5fe06233be55f3a8e9d8940a58c7b58017
Matheus Veiga <109769048+Matheusveiga@users.noreply.github.com>
2025-11-17 23:38:34 +0000

    front funcional

--- Backend/obj/Debug/net8.0/XDrones.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/XDrones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XDrones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875acc53d9a57afa41d11ee0ef03ccf719e2fa75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74eb8a400817b0c2b37d923c3d233dfa6f91f740")]
 [assembly: System.Reflection.AssemblyProductAttribute("XDrones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XDrones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
