882183fc7de7ef7c0ef05b509f21d4dd1f5c1df7
Carlos Eduardo <cadu888m@gmail.com>
2025-11-17 20:51:00 -0300

    Minhas alterações

--- FrontEnd/assets/js/ui.js
+++ FrontEnd/assets/js/ui.js
@@ -3,3 +3,8 @@ document.querySelectorAll("[data-include]").forEach(async (el) => {
     const content = await fetch(file).then(r => r.text());
     el.innerHTML = content;
 });
+//função de scroll aparecer a barra do header branco no site
+window.addEventListener("scroll", function () {
+    const topo = document.querySelector(".topo");
+    topo.classList.toggle("scrolled", window.scrollY > 50);
+});
