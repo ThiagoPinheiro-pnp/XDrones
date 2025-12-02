39f0e033e71d7f27924e58ec56c929a0d67a00a5
João Victor Lopes <155036709+Jhonies@users.noreply.github.com>
2025-12-02 00:49:05 +0000

    Adicionando saiba mais e começando a mudar as metricas de dinheiro em programcs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,9 +5,28 @@ using Microsoft.EntityFrameworkCore;
 using Backend.Data;
 using Backend.Services;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Define a cultura padrão para o Brasil
+var cultureInfo = new CultureInfo("pt-BR");
+
+// 2. Define as culturas suportadas
+var supportedCultures = new[] { cultureInfo };
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(cultureInfo);
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.RequestCultureProviders.Insert(0, new AcceptLanguageHeaderRequestCultureProvider());
+});
+
+// 3. Define a cultura para threads internas (importante para formatação)
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 
 // Configuration & DI
